2ae4d9204b423b2e8a252aeb5c8d5ce248c0b162
licb <545360100@qq.com>
2019-10-08 10:50:24 +0800

    动态引入antd

--- next.config.js
+++ next.config.js
@@ -1,65 +1,73 @@
+const path = require('path');
 
-
-const withLess = require("./next-plugins/next-less");
-const withCss = require("@zeit/next-css");
-const withImages = require("next-images");
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const webpack = require("webpack");
-// const Debug = require("debug");
-// const log = Debug("config:next-config");
-
-// const isProd = process.env.NODE_ENV === "production";
-
-// function inspect(object, options) {
-//   options = Object.assign({ depth: null }, options);
-//   return util.inspect(object, options);
-// }
+const withLess = require('./next-plugins/next-less');
+const withCss = require('@zeit/next-css');
+const withImages = require('next-images');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const webpack = require('webpack');
 
 let config = {
   // 编译过后的文件夹名称
-  distDir: "build",
+  distDir: 'build',
 
   // 只有服务器端才有的配置
   serverRuntimeConfig: {
-    serverOnly: "secret",
+    serverOnly: 'secret',
   },
 
   // 服务器和客户端都有的配置
   publicRuntimeConfig: {
-    serverAndClient: "public",
-    host: "127.0.0.1:3333",
+    serverAndClient: 'public',
+    host: '127.0.0.1:3333',
     // 可以用于 子域名 切换语言
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-    ? process.env.LOCALE_SUBPATHS
-    : 'none',
+    localeSubpaths:
+      typeof process.env.LOCALE_SUBPATHS === 'string' ? process.env.LOCALE_SUBPATHS : 'none',
   },
 
-  webpack: config => {
-    // DEBUG变量传递到client端，用于debug
+  webpack: (webpackConifg, { isServer }) => {
+    webpackConifg.resolve.alias['@'] = path.join(__dirname, '');
 
-    // log("init-webpack-config", inspect(config));
-
-    config.plugins.push(
+    webpackConifg.plugins.push(
       new webpack.DefinePlugin({
-        "process.env.DEBUG": JSON.stringify(process.env.DEBUG),
-      })
+        'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+      }),
     );
 
-    // log("full-webpack-config", inspect(config));
+    // 服务端antd样式
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...webpackConifg.externals];
+      webpackConifg.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
 
-    return config;
+      webpackConifg.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+    }
+
+    return webpackConifg;
   },
-
-  // 配置CDN
-  // crossOrigin: "anonymous",
-  // assetPrefix: isProd ? "https://cdn.mydomain.com" : "",
 };
 
 config = withLess({
   ...config,
+  // 修复less打包问题
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  },
   cssLoaderOptions: {
     importLoaders: 1,
-    localIdentName: "[local]_[hash:base64:5]",
+    localIdentName: '[local]_[hash:base64:5]',
   },
   // postcss配置
   postcssLoaderOptions: {},
@@ -72,16 +80,16 @@ config = withImages({ ...config });
 config = withBundleAnalyzer({
   ...config,
   // analyze
-  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
     server: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/server.html",
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
     },
     browser: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/client.html",
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
     },
   },
 });
--- pages/bi.js
+++ pages/bi.js
@@ -1,11 +1,21 @@
 import React from 'react';
 // import styles from "./home.module.less";
-import { i18n, Link, withTranslation } from '../i18n'
+import { Button } from 'antd';
+import { i18n, Link, withTranslation } from '../i18n';
 
+const Bi = ({ t }) => {
+  return (
+    <>
+      <div>{t('title')}</div>
+      <Button
+        onClick={() => {
+          console.log('sd');
+        }}
+      >
+        点击测试
+      </Button>
+    </>
+  );
+};
 
-const Bi = ({t}) => {
-  
-  return <div>{t('title')}</div>
-}
-
-export default withTranslation('bi')(Bi)
+export default withTranslation('bi')(Bi);
